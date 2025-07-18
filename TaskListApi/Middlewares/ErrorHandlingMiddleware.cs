d2681f3fb005e37a7be01ef524d53d94d8770e28
sergii.kovalenko <sergii.kovalenko@dashdevs.com>
2025-07-18 14:26:31 +0300

    Added Logging of Error's details in ErrorHandlingMiddleware

--- TaskListApi/Middlewares/ErrorHandlingMiddleware.cs
+++ TaskListApi/Middlewares/ErrorHandlingMiddleware.cs
@@ -1,15 +1,18 @@
 ﻿using Cortex.Mediator;
 using TaskListApi.Notifications;
+using Microsoft.Extensions.Logging;
 
 namespace TaskListApi.Middlewares;
 
 public class ErrorHandlingMiddleware
 {
     private readonly RequestDelegate _next;
+    private readonly ILogger<ErrorHandlingMiddleware> _logger;
 
-    public ErrorHandlingMiddleware(RequestDelegate next)
+    public ErrorHandlingMiddleware(RequestDelegate next, ILogger<ErrorHandlingMiddleware> logger)
     {
         _next = next;
+        _logger = logger;
     }
 
     public async Task InvokeAsync(HttpContext context)
@@ -20,6 +23,7 @@ public class ErrorHandlingMiddleware
         }
         catch (Exception ex)
         {
+            _logger.LogError(ex, "Unhandled exception for request {Method} {Path}", context.Request.Method, context.Request.Path);
             var mediator = context.RequestServices.GetRequiredService<IMediator>();
             await mediator.PublishAsync(new ErrorNotification(ex, context));
             context.Response.StatusCode = 500;
